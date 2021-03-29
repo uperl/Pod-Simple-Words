ef97dc9e215cabd06a72e7e3453eeb34db4aabfe
Graham Ollis <plicease@cpan.org>
2021-03-29 04:16:38 -0600

    some more

---
+++ corpus/Foo.pm
@@ -0,0 +1,36 @@
+package
+  Foo;
+
+use strict;
+use warnings;
+
+=encoding utf8
+
+=head1 NAME
+
+Foo - Some module eh?
+
+=head1 SYNOPSIS
+
+ use Foo;
+ foo();  # a comment
+
+=head1 DESCRIPTION
+
+Привет.  Here is some text.
+
+=head1 SEE ALSO
+
+=over 4
+
+=item L<some text|FFI::Platypus>
+
+=item L<the google|https://google.com>
+
+=item L<pod2yamlwords>
+
+=back
+
+=cut
+
+1;
